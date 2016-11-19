e5c37d292ef62d9e4ae20f8e0780bb39ee1f839d
Andrea Angella <angella.andrea@gmail.com>
2016-11-19 23:43:14 +0000

    Attemp to define the semantics of IAuthentication adding constact tests

---
+++ Syns.Tests/AuthenticationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using Syns.Tests.Stubs;
+
+namespace Syns.Tests
+{
+    public class AuthenticationTests
+    {
+        [Test]
+        public void Success()
+        {
+            var username = "user@me.com";
+            var password = "password";
+
+            var authentication = new InMemoryAuthentication();
+            authentication.WithUser(username, password);
+
+            var loggedUser = authentication.Login(username, password);
+
+            Assert.That(loggedUser, Is.EqualTo(username));
+        }
+
+        [Test]
+        public void UserDoesNotExist()
+        {
+            var username = "user@me.com";
+            var password = "password";
+
+            var authentication = new InMemoryAuthentication();
+            authentication.WithoutUser(username);
+
+            var loggedUser = authentication.Login(username, password);
+
+            Assert.IsNull(loggedUser);
+        }
+
+        [Test]
+        public void UserExistButPassowordIsWrong()
+        {
+            var username = "user@me.com";
+            var password = "password";
+
+            var authentication = new InMemoryAuthentication();
+            authentication.WithUserButDifferentPassword(username, password);
+
+            var loggedUser = authentication.Login(username, password);
+
+            Assert.IsNull(loggedUser);
+        }
+    }
+}
---
+++ Syns.Tests/Stubs/InMemoryAuthentication.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Syns.Tests.Stubs
+{
+    public class InMemoryAuthentication : IAuthentication
+    {
+        private readonly Dictionary<string, string> users = new Dictionary<string, string>();
+
+        public string Login(string username, string password)
+        {
+            if (users.Keys.Contains(username) && users[username] == password)
+            {
+                return username;
+            }
+
+            return null;
+        }
+
+        public void WithUser(string username, string password)
+        {
+            users.Add(username, password);
+        }
+
+        public void WithoutUser(string username)
+        {
+            users.Add("Different " + username, "irrelevant passowrd");
+        }
+
+        public void WithUserButDifferentPassword(string username, string password)
+        {
+            users.Add(username, "Different " + password);
+        }
+    }
+}
