adb1d310a40e5b46204c43f273ec2c5d17e577b3
Andrea Angella <angella.andrea@gmail.com>
2016-11-20 08:22:21 +0000

    Test TodaySynsLeft. It's clear we need a User abstraction now with the concept of profile

--- Syns.Tests/TodaySynsTests.cs
+++ Syns.Tests/TodaySynsTests.cs
@@ -34,5 +34,33 @@ namespace Syns.Tests
             authentication.Received(1).Login(username, password);
             synsStore.Received(1).GetTodaySyns(username);
         }
+
+        [TestCase(25, 10.5, 14.5)]
+        [TestCase(25, 25, 0)]
+        [TestCase(25, 27, 0)]
+        public void TodaySynsLeft(decimal synsAllowance, decimal todaySyns, decimal synsLeft)
+        {
+            // Arrange
+            var username = "username@me.com";
+            var password = "password";
+
+            var authentication = Substitute.For<IAuthentication>();
+            authentication
+                .Login(username, password)
+                .Returns(username);
+
+            var synsStore = Substitute.For<ISynsStore>();
+            synsStore
+                .GetTodaySyns(username)
+                .Returns(todaySyns);
+
+            // Act
+            var application = new Application(authentication, synsStore);
+            application.Login(username, password);
+            application.SetSynsAllowance(synsAllowance);
+
+            // Assert
+            Assert.That(application.TodaySynsLeft, Is.EqualTo(synsLeft));
+        }
     }
 }
--- Syns/Application.cs
+++ Syns/Application.cs
@@ -1,10 +1,13 @@
-﻿namespace Syns
+﻿using System;
+
+namespace Syns
 {
     public class Application
     {
         private readonly IAuthentication m_Authentication;
         private readonly ISynsStore m_SynsStore;
         private string m_LoggedUserName;
+        private decimal m_SynsAllowance;
 
         public Application(IAuthentication authentication, ISynsStore synsStore)
         {
@@ -25,6 +28,18 @@
         public decimal TodaySyns()
         {
             return m_SynsStore.GetTodaySyns(m_LoggedUserName);
+        }
+
+        public void SetSynsAllowance(decimal synsAllowance)
+        {
+            m_SynsAllowance = synsAllowance;
+        }
+
+        public decimal TodaySynsLeft()
+        {
+            var synsLeft = m_SynsAllowance - TodaySyns();
+
+            return synsLeft > 0 ? synsLeft : 0;
         }
     }
 }
